f0bfbc4448c17c9c4ed27f1455831b512d93c5a6
JoanMRomero <joanmr87@gmail.com>
2021-05-10 11:46:46 -0300

    agrego post, sin conexion aun

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require ("express");
 const { PORT } = require("./config");
 const app = express();
 
-const DB = [];
+app.use(express.json());
+
+const DB = [{id: 1, name: 'Dante', age: 3}];
 
 app.get("/api/users", (req, res) => {
     res.json(DB);
+});
+
+app.post("/api/users", (req, res) => {
+    const user = {
+        id: req.body.id,
+        name: req.body.name,
+        age: req.body.age,
+    };
+    res.json(user);
 });
 
 app.listen(PORT, () => {
